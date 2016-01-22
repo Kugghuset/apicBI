28c08263b8408172b9b011e16a4f1e102f81da15
Kristoffer Ostlund <kristoffer.ostlund@gmail.com>
2016-01-22 16:24:51 +0100

    Add schedules to server.js
    This actually only is the scheduling, it doesn't do the clearing of tables

--- server.js
+++ server.js
@@ -4,8 +4,36 @@ var env = require('node-env-file');
 var later = require('later');
 env(global.root + '/.env');
 
-var DB2BI = require(global.root + '/controllers/db2bi.js');
+var DB2BI = require('./controllers/db2bi');
+var PowerBI = require('./lib/powerBi');
 
-later.setInterval(function() {
-    DB2BI.read();
-}, later.parse.recur().every(10).second());
+
+// Schedule which will run every 10 seconds
+var every10Seconds = later.parse.recur()
+    .every(10).second();
+
+
+// Schedule which will run at the change och day, every day.
+var everyStartOfDay = later.parse.recur()
+    .first().hour();
+
+
+// Schedule which will run at the change of weeks.
+var everyStartOfWeek = later.parse.recur()
+    .on(2).dayOfWeek()
+    .first().hour();
+
+// Regular update
+later.setInterval(DB2BI.read, every10Seconds);
+
+// Daily clearing of the daily table
+later.setInterval(function () {
+    // Clear the day_per_agent table in ApicBI
+    // INSERT HERE
+}, everyStartOfDay);
+
+// Weekly clearing of the weekly table
+later.setInterval(function () {
+    // Clear the week_per_agent table in ApicBI
+    // INSERT HERE
+}, everyStartOfWeek);
