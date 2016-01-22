bd8dd2336280917188aa2e9bfb23e1d87485dccf
Hannes Dahlberg <hannes@dwik.se>
2016-01-22 14:42:04 +0100

    Init and PowerBi updates - New init with reinit and specific table to reinit command - Updates to tableExists function in PowerBi, fixing error message - reInit in PowerBi now takes on table to update instead of whole schema

--- init.js
+++ init.js
@@ -3,6 +3,8 @@ global.root = __dirname;
 var env = require('node-env-file');
 env(global.root + '/.env');
 
+var _ = require('lodash');
+
 var ArgValues = require(global.root + '/lib/argValues.js');
 var AzureAuth = require(global.root + '/lib/azureAuth.js');
 var PowerBi = require(global.root + '/lib/powerBi.js');
@@ -12,8 +14,14 @@ var azure = new AzureAuth();
 new ArgValues(['dataset']).then(function(args) {
     azure.getToken().then(function(data) {
         var powerBi = new PowerBi(data.token);
-        powerBi.init(args.dataset).then(function(result) {
-            console.log(result.message);
-        }).catch(function(error) { console.log('Something went wrong', error); });
+        if(!_.isUndefined(args.reinit)) {
+            powerBi.reInit(args.dataset, !_.isUndefined(args.table) ? args.table : undefined).then(function(result) {
+                console.log(result);
+            }).catch(function(error) { Error.print('Something went wrong', error); });
+        } else {
+            powerBi.init(args.dataset).then(function(result) {
+                console.log(result.message);
+            }).catch(function(error) { console.log('Something went wrong', error); });
+        }
     }).catch(function(error) { console.log('Something went wrong', error); });
 }).catch(function(error) { console.log('Something went wrong', error); });
