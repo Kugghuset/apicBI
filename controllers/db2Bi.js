57824f55ebca8b485840f9557160eea6797fbe1e
Konrad Leffler <ko0320852@cidronpay.com>
2016-01-22 09:34:38 +0100

    Controller and library updates - Controller has been updated to only insert new rows using timestamp saved to file and comparing to DB - Init has new command for reinitiate an existing dataset (update table structure) - The ApicBI dataset structure has been updated to match SQL-query results - Modified the reinit command of PowerBI model to wait for each table promise to finish - listTables method in PowerBI model now returns dataset name as well as dataset ID but only if it was provided (eg. dataset lookup was made) - table schema updated and table clear methods in PowerBI model now resolves with current table name in message - New clean command to clear all tables in a dataset

--- controllers/db2Bi.js
+++ controllers/db2Bi.js
@@ -8,10 +8,7 @@ var path = require('path');
 var moment = require('moment');
 var _ = require('lodash');
 
-
 var database = require(global.root + '/configs/database.js');
-
-var datasetId = 'c85f937b-f614-4fa9-95bf-3b07e757c21b';
 
 // Init only needed once
 sql.setDefaultConfig(database.ic);
@@ -33,36 +30,65 @@ DB2BI.read = function() {
             ? fs.readFileSync(path.resolve(global.root, 'assets/lastUpdated.txt'), 'utf8')
             : moment().startOf('week').valueOf();
         
-        sql.execute({
-            query: sql.fromFile('../sql/poll_per_agent_call_length_this_week.sql'),
-            params: {
-                LastUpdate: {
-                    type: sql.DATETIME2,
-                    val: new Date(parseInt(lastUpdated))
+        function getQuery() {
+            return sql.execute({
+                query: sql.fromFile('../sql/poll_per_agent_call_length_this_week.sql'),
+                params: {
+                    LastUpdate: {
+                        type: sql.DATETIME2,
+                        val: new Date(parseInt(lastUpdated))
+                    }
                 }
+            });
+        }
+        
+        Promise.settle([
+            powerBi.datasetExists('ApicBI'),
+            getQuery()
+        ])
+        .then(function (data) {
+            
+            // Get the datasetId from the response object
+            var datasetId = _.attempt(function () { return data[0].value().dataset.id });
+            if (_.isError(datasetId)) {
+                // return early, do something about it?
+                return console.log('Could not get datasetId');
             }
-        })
-        .then(function (recordset) {
+            
+            // Get the recordset
+            var recordset = _.attempt(function () { return data[1].value(); });
+            if (_.isError(recordset)) {
+                return console.log('Could not get recordset from the database.')
+            }
             
             // Get the latest value from the table, if any
             var latest = _.map(recordset).sort(function (a, b) {
                 return moment(a.I3TimeStampGMT).isAfter(b.I3TimeStampGMT);
             }).pop();
             
-            
             var todayOnly;
             
             // Check if the date is the very same as the start of this week
             // this should only work on first boot.
             if (parseInt(lastUpdated) === moment().startOf('week').valueOf()) {
-                todayOnly = _.filter(recordset, function (item) { return moment().startOf('day').isBefore(item.I3TimeStampGMT); });
+                todayOnly = _.chain(recordset)
+                    .filter(function (item) { return moment().startOf('day').isBefore(item.I3TimeStampGMT); })
+                    .map(function (row) { return _.omit(row, 'I3TimeStampGMT'); })
+                    .value();
             } else {
-                todayOnly = recordset;
+                todayOnly = _.map(recordset, function (row) { return _.omit(row, 'I3TimeStampGMT'); });
             }
+            
+            recordset = _.map(recordset, function (row) { return _.omit(row, 'I3TimeStampGMT'); });
             
             // Save the timestamp for future use, if there is one
             if (latest && latest.I3TimeStampGMT) {
                 fs.writeFileSync(filePath, latest.I3TimeStampGMT.getTime());
+            }
+            
+            // recordset will allways be the same or greater than todayOnly, so it's valid to only check it's length;
+            if (recordset && recordset.length) {
+                console.log('New data found! Sending at: ' + moment().format('YYYY-MM-DD HH:mm'));
             }
             
             // Iterate over data for the current day and the current week
@@ -73,14 +99,14 @@ DB2BI.read = function() {
                     
                     // Table names will be be prefixed with 'day_' or 'week_'
                     powerBi.addRows(datasetId, [key,'per_agent'].join('_'), value).then(function(result) {
-                        console.log([key,'per_agent'].join('_') + ' sent ' + value.length + ' rows.');
+                        console.log([key,'per_agent'].join('_') + ' sent ' + value.length + ' rows. ' + moment().format('YYYY-MM-DD HH:mm'));
                     }).catch(function(error) {
                         console.log(error);
                     });
                     
                     // Table names will be be prefixed with 'day_' or 'week_'
                     powerBi.addRows(datasetId, [key,'aggregated'].join('_'), value).then(function(result) {
-                        console.log([key,'aggregated'].join('_') + ' sent ' + value.length + ' rows.');
+                        console.log([key,'aggregated'].join('_') + ' sent ' + value.length + ' rows. ' + moment().format('YYYY-MM-DD HH:mm'));
                     }).catch(function(error) {
                         console.log(error);
                     });
