4d244a7c29f1d78bff2e4c79ecbe4d5cf888a087
Konrad Leffler <ko0320852@cidronpay.com>
2016-02-19 18:01:58 +0100

    Start adding support for user statuses

--- controllers/icwsController.js
+++ controllers/icwsController.js
@@ -7,6 +7,12 @@ var ICWS = require('../lib/icws');
 var _icws;
 var _interval;
 
+// All users in the system
+var _users = [];
+
+// Users grouped by their state
+var _usersByState = {};
+
 /**
  * Polls the messaging route for new data
  * and handles new data for various endpoints.
@@ -20,13 +26,49 @@ function poll() {
         
         var dataArr = res.body || [];
         
-        console.log(JSON.stringify(dataArr, null, 4));
+        var modifiedUsers = _.find(dataArr, function (data) { return _.get(data, '__type') === 'urn:inin.com:configuration.people:usersMessage' });
+        
+        if (modifiedUsers) {
+            
+            modifiedUsers.added = _.map(modifiedUsers.added, function (user) { return _.get(user, 'configurationId.id'); });
+            modifiedUsers.changed = _.map(modifiedUsers.changed, function (user) { return _.get(user, 'configurationId.id'); });
+            modifiedUsers.removed = _.map(modifiedUsers.removed, function (user) { return _.get(user, 'configurationId.id'); });
+            
+            // Filter out any removed users
+            _users = _.filter(_users, function (user) { return ~modifiedUsers.removed.indexOf(user); })
+            // Combine the added users with the existing users
+            _users = _.uniq(_users.concat(modifiedUsers.added));
+            
+            // Update or create the queue subscription
+            userStatusSub('subscribe', _users)
+            .catch(function (res) { console.log(res) })
+            dataArr = _.filter(dataArr, function (data) { return _.get(data, '__type') != 'urn:inin.com:configuration.people:usersMessage' });
+        }
+        
+        var statusUpdates = _.find(dataArr, function (data) { return _.get(data, '__type') === 'urn:inin.com:status:userStatusMessage'; });
+        
+        if (statusUpdates) {
+            
+            var updatedUsers = _.map(statusUpdates.userStatusList, function (userStatus) {
+                return _.get(userStatus, 'userId');
+            });
+            
+            updateUserStatuses(updatedUsers);
+            
+        }
+        
+        if (modifiedUsers && _users.length && _.isEqual({}, _usersByState)) {
+            updateUserStatuses(_users);
+        }
+        
+        console.log(JSON.stringify((dataArr || {}), null, 4));
         
     });
 }
 
 /**
  * Creates a subscription to *path* with *content*.
+ * _L
  * 
  * @param {String} path
  * @param {Object} body
@@ -56,7 +98,7 @@ function userSub(action, subId) {
     
     subId = !_.isUndefined(subId)
         ? subId
-        : 1;
+        : 'kugguset-1';
     
     var path = 'messaging/subscriptions/configuration/users/:id'
         .replace(':id', subId);
@@ -89,7 +131,7 @@ function workStationSub(action, subId) {
     
     subId = !_.isUndefined(subId)
         ? subId
-        : 1;
+        : 'kugguset-1';
     
     var path = 'messaging/subscriptions/configuration/workgroups/:id'
         .replace(':id', subId)
@@ -111,6 +153,116 @@ function workStationSub(action, subId) {
             ],
             rightsFilter: 'view'
         });
+}
+
+/**
+ * 
+ * 
+ * @param {String} action Should be either 'subscribe' or 'unsubscribe'
+ * @param {Array} _users
+ * @return {Promise}
+ */
+function userStatusSub(action, _users) {
+    
+    var subPath = 'messaging/subscriptions/status/user-statuses';
+    
+    return /unsub/i.test(action)
+        ? unsubscribe(subPath)
+        : subscribe(subPath, {
+            userIds: _.flatten(_users),
+            userStatusProperties: [
+                'personalInformationProperties.companyName',
+                'personalInformationProperties.country',
+                'personalInformationProperties.departmentName',
+                'personalInformationProperties.emailAddress',
+                'personalInformationProperties.givenName',
+                'personalInformationProperties.surname'
+            ]
+        });
+}
+
+/**
+ * Subscribes to all queus for *_users*
+ * 
+ * NOT WORKING
+ * 
+ * @param {String} action Should be either 'subscribe' or 'unsubscribe'
+ * @param {String|Number} subId
+ * @param {Array} _users The user ids (firstname.lastname) to listen for.
+ * @return {Promise}
+ */
+function queueSub(action, subId, _users) {
+    
+    subId = !_.isUndefined(subId)
+        ? subId
+        : 'kugguset-1';
+    
+    var queueIds = _.map(_users, function (user) { return { queueType: 1, queueName: user }; })
+    
+    var subPath = 'messaging/subscriptions/queues/:id'
+        .replace(':id', subId)
+    
+    return /unsub/i.test(action)
+        ? unsubscribe(subPath)
+        : subscribe(subPath, {
+            
+            queueIds: queueIds,
+            attributeNames: [
+                'Eic_State',
+                'Eic_ConnectDurationTime'
+            ],
+            
+            rightsFilter: 'view'
+            
+        });
+    
+}
+
+/**
+ * Updates users in _usersByState which are in userIds
+ * 
+ * Docs here: https://developer.inin.com/documentation/Documents/ICWS/WebHelp/icws/(sessionId)/configuration/users/index.htm#get
+ * 
+ * @param {String} userIds
+ * @return {Promise} -> {Array}
+ */
+function updateUserStatuses(userIds) {
+    
+    var userUrl = [
+        'configuration/users/:id',
+        [
+            'rightsFilter=view',
+            'select=statusText,lastModifiedDate,workgroups'
+        ].join('&')
+    ].join('?')
+    
+    var promises = _.map(userIds, function (userId) {
+        return _icws.get(userUrl.replace(':id', userId));
+    });
+    
+    Promise.all(_.map(promises, function (promise) { return promise.reflect(); }))
+    .then(function (data) {
+        
+        var users = _.map(data, function (val) { return val.isRejected() ? val.reason() : (val.value() || {}).body; });
+        
+        _.chain(users)
+            .map(function (user) {
+                return {
+                    id: _.get(user, 'configurationId.id'),
+                    name: _.get(user, 'configurationId.displayName'),
+                    status: _.get(user, 'statusText'),
+                    lastModifiedDate: _.get(user, 'lastModifiedDate'),
+                    workgroups: _.get(user, 'workgroups')
+                };
+            })
+            .groupBy('status')
+            .tap(function (items) {
+                console.log(JSON.stringify(items, null, 4));
+            })
+            .value();
+        
+    })
+    
 }
 
 /***************
@@ -144,10 +296,13 @@ function init() {
 function setupSubscriptions() {
     return new Promise(function (resolve, reject) {
         
-        var promises = [ userSub('subscribe', 1), workStationSub('subscribe', 1) ];
+        var promises = [ userSub('subscribe', 'kugguset-1'), workStationSub('subscribe', 'kugguset-1') ];
         
         Promise.all(_.map(promises, function (promise) { return promise.reflect(); }))
         .then(function (data) {
+            
+            // console.log(_.map(data, function (val) { return val.isRejected() ? val.reason() : val.value(); }));
+            
             resolve(_.map(data, function (val) { return val.isRejected() ? undefined : val.value(); }))
         });
         
@@ -213,6 +368,11 @@ function run() {
         .then(function (data) {
             
             startPolling();
+            
+            // _icws.get('configuration/defaults/queue-column')
+            // .then(function (res) {
+            //   console.log(res.body);
+            // })
             
             resolve(_icws);
         })
