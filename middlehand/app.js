d35177642fd8099c80a0799ac1f68e91c1f745de
Kristoffer Östlund <kristoffer.ostlund@gmail.com>
2016-08-30 10:22:36 +0200

    Try out filtered changes query

--- middlehand/app.js
+++ middlehand/app.js
@@ -8,18 +8,39 @@ var config = require('./config');
 
 var models = require('./models/models');
 
+var Interaction = models.models.Interaction;
+
 db.init({ db: 'icws' })
 .then(models.init)
 .then(function () {
-    console.log('Listening for changes in interaction')
-    models.models.Interaction.listen(function (err, value) {
-        if (err) { console.log(err); }
-        if (value) { console.log(value); }
-    });
+    // console.log('Listening for changes in interaction')
 
-    console.log('Listening for changes in agent')
-    models.models.Agent.listen(function (err, value) {
-        if (err) { console.log(err); }
-        if (value) { console.log(value); }
-    });
+    Interaction.filter(
+        r.row('inQueue')
+    )
+    .pluck(['inQueue', 'queueTime', 'correctedQueueTime'])
+    .run(db.conn(), function (err, cursor) {
+        if (err) { console.log(err) }
+        else if (cursor) { cursor.each(function (err, item) { console.log(!!err ? err : item); }) }
+    })
+
+    Interaction.filter(
+        r.row('inQueue')
+    )
+    .pluck(['inQueue', 'queueTime', 'correctedQueueTime'])
+    .changes().run(db.conn(), function (err, cursor) {
+        if (err) { console.log(err) }
+        else if (cursor) { cursor.each(function (err, item) { console.log(!!err ? err : item); }) }
+    })
+
+    // models.models.Interaction.listen(function (err, value) {
+    //     if (err) { console.log(err); }
+    //     if (value) { console.log(value); }
+    // });
+
+    // console.log('Listening for changes in agent')
+    // models.models.Agent.listen(function (err, value) {
+    //     if (err) { console.log(err); }
+    //     if (value) { console.log(value); }
+    // });
 });
